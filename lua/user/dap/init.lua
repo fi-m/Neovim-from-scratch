e58720669b975ba57dfcdb7a6dad10edd6a8413b
fi-m <mattsson.filip@hotmail.com>
2022-07-04 23:58:56 +0200

    misc: Added some info

--- lua/user/dap/init.lua
+++ lua/user/dap/init.lua
@@ -2,5 +2,7 @@ local status_ok, _ = pcall(require, "dap")
 if not status_ok then
 	return
 end
+-- TODO: Add more configurations
 
+-- Python, need venv with debugpy installed
 require('dap-python').setup('~/.venvs/debugpy/bin/python')
