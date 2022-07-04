3372bec7af6a10d0ae4ead4a48cb1b05cd5413fe
fi-m <mattsson.filip@hotmail.com>
2022-07-04 23:57:09 +0200

    feat: Added debugging

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -104,6 +104,9 @@ return packer.startup(function(use)
 	-- NVIM-Remote-Containers
 	use("jamestthompson3/nvim-remote-containers")
 
+-- Debugging
+  use("mfussenegger/nvim-dap")
+  use("mfussenegger/nvim-dap-python")
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
