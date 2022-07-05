becb947f30449147baa56cb84a78b1a285c386f0
fi-m <mattsson.filip@hotmail.com>
2022-07-05 21:48:27 +0200

    fix: Dap-ui

--- lua/user/dap/init.lua
+++ lua/user/dap/init.lua
@@ -28,5 +28,16 @@ vim.fn.sign_define("dapbreakpoint", dap_breakpoint.error)
 vim.fn.sign_define("dapstopped", dap_breakpoint.stopped)
 vim.fn.sign_define("dapbreakpointrejected", dap_breakpoint.rejected)
 ]]
+local dapui = require "dapui"
+  dapui.setup {} -- use default
+  dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+  end
+  dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+  end
+  dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+  end
 -- python, need venv with debugpy installed
 require("dap-python").setup("~/.venvs/debugpy/bin/python")
