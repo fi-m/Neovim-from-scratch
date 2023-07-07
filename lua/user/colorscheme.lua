b9a5b2cd1d3f9cfc21c76bb21a05372e827441cc
fi-m <mattsson.filip@hotmail.com>
2023-07-07 22:16:23 +0200

    stuff

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,70 +1,20 @@
--- vim.cmd [[
--- try
---   colorscheme monokai_soda
--- catch /^Vim\%((\a\+)\)\=:E185/
---   colorscheme default
---   set background=dark
--- endtry
--- ]]
---
-require('monokai').setup { palette = require('monokai').soda }
+require('github-theme').setup({
+    options = {
+    styles = {
+      comments = 'italic',
+      keywords = 'bold',
+      types = 'italic,bold',
+    },
+    modules = {
+      cmp = true,
+      gitsigns = true,
+      telescope = true,
+      whichkey = true,
+      treesitter = true,
+      dapui = true,
+      fzf = true,
+    },
+  }
+})
 
--- require("github-theme").setup({
--- 	function_style = "italic",
--- 	sidebars = { "qf", "vista_kind", "terminal", "packer" },
--- 
--- 	-- Change the "hint" color to the "orange" color, and make the "error" color bright red
--- 	colors = { hint = "orange", error = "#ff0000" },
--- })
-
--- Load and setup function to choose plugin and language highlights for codeschool
--- require('lush')(require('codeschool').setup({
---   plugins = {
---     -- "buftabline",
---     -- "coc",
---     "cmp", -- nvim-cmp
---     -- "fzf",
---     -- "gitgutter",
---     "gitsigns",
---     "lsp",
---     -- "lspsaga",
---     -- "nerdtree",
---     -- "netrw",
---     "nvimtree",
---     -- "neogit",
---     "packer",
---     -- "signify",
---     -- "startify",
---     -- "syntastic",
---     "telescope",
---     "treesitter"
---   },
---   langs = {
---     "c",
---     "clojure",
---     "coffeescript",
---     "csharp",
---     "css",
---     "elixir",
---     "golang",
---     "haskell",
---     "html",
---     "java",
---     "js",
---     "json",
---     "jsx",
---     "lua",
---     "markdown",
---     "moonscript",
---     "objc",
---     "ocaml",
---     "purescript",
---     "python",
---     "ruby",
---     "rust",
---     "scala",
---     "typescript",
---     "viml",
---     "xml"
---   }
--- }))
+vim.cmd('colorscheme github_dark')
